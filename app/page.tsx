3836ca49a8aa68bf02da8805803e2fb5ac58e87c
unknown <kdeepa279@gmail.com>
2025-05-21 15:38:34 +0530

    form updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <form action="https://registeration-backend-vercel-git-main-deepas-projects-0a652578.vercel.app/" method="POST">
+    <form action="https://registeration-backend-vercel-git-main-deepas-projects-0a652578.vercel.app/api/register" method="POST">
+
 
       <h2>Register</h2>
 
